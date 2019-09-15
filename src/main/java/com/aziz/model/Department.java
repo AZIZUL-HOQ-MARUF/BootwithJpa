8ec8b23d885f8380649014a8cbc99f1accdd6bd0
Azizul <azizulmaruf@gmail.com>
2019-09-15 16:22:03 +0600

    Added Bidirectional rltn OneToMany and ManyToOne btwn Employee & Dept

--- src/main/java/com/aziz/model/Department.java
+++ src/main/java/com/aziz/model/Department.java
@@ -1,35 +1,48 @@
 package com.aziz.model;
 
 import java.io.Serializable;
+import java.util.Collection;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="Department")
-public class Department implements Serializable{
-	@Id
-	private long id;
-	
-	@Column(name="NAME")
-	private String name;
+@Table(name = "Department")
+public class Department implements Serializable {
 
-	public long getId() {
-		return id;
-	}
+    @Id
+    private long id;
 
-	public void setId(long id) {
-		this.id = id;
-	}
+    @Column(name = "NAME")
+    private String name;
+    
+    @OneToMany(mappedBy = "department")
+    private Collection<Employee> employee;
 
-	public String getName() {
-		return name;
-	}
+    public Collection<Employee> getEmployee() {
+        return employee;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setEmployee(Collection<Employee> employee) {
+        this.employee = employee;
+    }
 
-	
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }
